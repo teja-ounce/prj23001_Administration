47da6a41019f0ce124b051d2e1c57845de22703c
teja-ounce <teja.somala@proounce.com>
2024-07-04 19:24:44 +0530

    Changes in Hooks class

--- src/test/java/com/admin/stepDefinition/Hooks.java
+++ src/test/java/com/admin/stepDefinition/Hooks.java
@@ -1,11 +1,14 @@
 package com.admin.stepDefinition;
 
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 
 import com.admin.utils.DriverFactory;
 
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
 
 public class Hooks {
 	private WebDriver driver;
@@ -16,7 +19,20 @@ public class Hooks {
 	}
 
 	@After
-	public void tearDown() {
-		DriverFactory.quitDriver();
+	public void tearDown(Scenario scenario) {
+		if (scenario.isFailed()) {
+			// Take screenshot if the scenario fails
+			if (driver instanceof TakesScreenshot) {
+				final byte[] screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
+				scenario.attach(screenshot, "image/png", "Screenshot");
+				DriverFactory.quitDriver();
+			}
+		} else {
+			DriverFactory.quitDriver();
+		}
+	}
+
+	public WebDriver getDriver() {
+		return driver;
 	}
 }
